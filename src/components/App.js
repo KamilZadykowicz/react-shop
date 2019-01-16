d2fcae2537a58fb03ab5008384bd8cfcee2556b6
Kamil <kamil.zadykowicz@adsira.net>
2019-01-16 11:35:30 +0100

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import './App.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="container">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Edit <code>src/App.js</code> and save to reload!
           </p>
           <a
             className="App-link"
